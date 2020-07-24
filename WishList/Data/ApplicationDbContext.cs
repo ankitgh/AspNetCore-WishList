1c208ff21e8511853e79172c84baed2984997c67
Ankit Gupta (AZURE-IDENTITY) <ankgupt@microsoft.com>
2020-07-23 22:20:38 -0700

    Item class

---
+++ WishList/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WishList.Models;
+
+namespace WishList.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Item> Items { get; set; }
+        public ApplicationDbContext([NotNullAttribute] DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+}
