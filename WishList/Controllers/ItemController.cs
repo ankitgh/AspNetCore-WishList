2a91afe1b07b0e7e1e3335513451b7c9d5263ddf
Ankit Gupta (AZURE-IDENTITY) <ankgupt@microsoft.com>
2020-07-24 14:49:50 -0700

    controller

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+
+        // GET: ItemController
+        public IActionResult Index()
+        {
+            return View("Index", _context.Items);
+        }
+
+        // GET: ItemController/Create
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+
+        // POST: ItemController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Item item)
+        {
+            _context.Items.Add(item);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        // GET: ItemController/Delete/5
+        public IActionResult Delete(int id)
+        {
+            Item item = _context.Items.Find(id);
+            _context.Items.Remove(item);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
